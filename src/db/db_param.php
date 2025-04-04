74ef1c7017177c6e1e0677a369811817addfa778
synnfall <synnfall@gmail.com>
2025-04-04 16:35:48 +0200

    test serv

--- src/db/db_param.php
+++ src/db/db_param.php
@@ -1,5 +1,12 @@
 <?php
-$hosts = 'localhost';
-$user = "";
-$passwd = "";
-$db_name = "";
+$env = parse_ini_file('../.env');
+
+if(! ( isset($env["DB_HOST"]) && isset($env["DB_NAME"]) && isset($env["USER"]) && isset($env["PASSWORD"]) && isset($env["PORT"])) ) exit(".env mal complété ou problème lors de son chargement");
+
+$hosts = $env["DB_HOST"];
+$user = $env["USER"];
+$passwd = $env["PASSWORD"];
+$db_name = $env["DB_NAME"];
+$port = $env["PORT"];
+
+unset($env);
