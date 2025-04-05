ec90d239abaeef792f5ea90934d7176b680fe83b
bounci <lulubouncer@gmail.com>
2025-04-05 22:17:30 +0200

    update index.html + creation styles.css

--- src/public/index.php
+++ src/public/index.php
@@ -4,3 +4,15 @@ include "../db/db_connect.php";
 echo "coucou";
 var_dump($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JPEG</title>
+</head>
+<body>
+  
+</body>
+</html>
