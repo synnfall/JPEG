baa4797953e5f5083a408f51b9df090b2b61a179
bounci <lulubouncer@gmail.com>
2025-04-07 00:09:33 +0200

    fin creation page login, (manque plus que quelques transitions et les variables php )

---
+++ src/public/scripts/login.js
@@ -0,0 +1,19 @@
+
+
+toggle_eye=document.querySelector(".toggle-eye");
+toggle_eye.addEventListener("click",hide_and_show_password);
+
+function hide_and_show_password(){
+  let passwordInput = document.querySelector("#password");
+  let eyeIcon = document.querySelector(".toggle-eye");
+
+  if (passwordInput.type === "password"){
+    passwordInput.type = "text";
+    eyeIcon.src = "./img/icons/Show.png";
+    eyeIcon.classList.add('rotated');
+  } else{
+    passwordInput.type = "password";
+    eyeIcon.src = "./img/icons/Hide.png";
+    eyeIcon.classList.remove('rotated');
+  }
+}
