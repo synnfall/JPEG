31fc391368bd52e1c1f311d8c90a2f4f8460aa7d
Bouncii <lulubouncer@gmail.com>
2025-04-07 17:44:36 +0200

    implémentation toggle signup/login

--- src/public/scripts/login.js
+++ src/public/scripts/login.js
@@ -18,9 +18,22 @@ function hide_and_show_password(){
   }
 }
 
-remeberme=document.querySelector(".remember input");
-rememberme.addEventListener("click",rememberme_or_not);
+bouton_toggle_login_into_signup=document.querySelector(".switch_login_into_signup");
+bouton_toggle_login_into_signup.addEventListener("click",toggle_login_into_signup);
 
-function rememberme_or_not(){
-  
+function toggle_login_into_signup(){
+  let login_container=document.querySelector("#login_container");
+  let signup_container=document.querySelector("#signup_container");
+  login_container.classList.add('hidden');
+  signup_container.classList.remove('hidden');
+}
+
+bouton_toggle_signup_into_login=document.querySelector(".switch_signup_into_login");
+bouton_toggle_signup_into_login.addEventListener("click",toggle_signup_into_login);
+
+function toggle_signup_into_login(){
+  let login_container=document.querySelector("#login_container");
+  let signup_container=document.querySelector("#signup_container");
+  signup_container.classList.add('hidden');
+  login_container.classList.remove('hidden');
 }
