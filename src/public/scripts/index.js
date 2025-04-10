4499e91a3ab20b1cd30e8b7e6ac00c356d359931
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2025-04-10 21:41:03 +0200

    js var assignment fix + css

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -9,7 +9,7 @@ debug=true;
  */
 
 if (debug) {
-    data_caroussel = [
+    var data_caroussel = [
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. blabla",
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. blabla",
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. blabla",
@@ -17,7 +17,7 @@ if (debug) {
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. blabla"
     ];
 
-    data_classement = [
+    var data_classement = [
         {
             "pseudo" : "Aline",
             "points" : 1234,
@@ -46,8 +46,8 @@ if (debug) {
         },     
     ];
 } else {
-    data_caroussel = [];
-    data_classement = [];
+    var data_caroussel = [];
+    var data_classement = [];
 }
 /**
  * ONLOAD
@@ -71,23 +71,23 @@ function html_onload() {
  */
 
 function add_message_bienvenue() {
-    message = get_message_bienvenue();
-    h1 = html_message_bienvenue(message);
+    let message = get_message_bienvenue();
+    let h1 = html_message_bienvenue(message);
     
-    div_titre = document.querySelector(".titre");
+    let div_titre = document.querySelector(".titre");
     div_titre.appendChild(h1);
 }
 
 function html_message_bienvenue(message) {
-    text = document.createTextNode(message);
-    h1 = document.createElement("h1");
+    let text = document.createTextNode(message);
+    let h1 = document.createElement("h1");
 
     h1.appendChild(text);
     return h1
 }
 
 function remove_message_bienvenue() {
-    div_bienvenue = document.querySelector(".titre")
+    let div_bienvenue = document.querySelector(".titre")
     div_bienvenue.children["0"].remove();
 }
 
@@ -97,7 +97,7 @@ function change_message_bienvenue() { // Non utilisée, a remove si besoins.
 }
 
 function get_message_bienvenue() {
-    messages = [
+    let messages = [
         "EsT-cE qUe cE sItE eST cOnvErtIt eN PdF ?!?",
         "Aline was here",
         ":3",
@@ -119,7 +119,7 @@ function get_message_bienvenue() {
         "J.P.E.G.",
     ];
 
-    const index_message_random = Math.floor(Math.random() * messages.length);
+    let index_message_random = Math.floor(Math.random() * messages.length);
 
     if (debug) {
         console.log(index_message_random, messages[index_message_random]);
@@ -137,13 +137,13 @@ function get_message_bienvenue() {
 
 function add_carrousel_jeux(data) {
 
-    div_caroussel = document.querySelector(".caroussel_jeux");
+    let div_caroussel = document.querySelector(".caroussel_jeux");
     
     if (data.length > 0) {
-        table = html_carrousel_jeux(data);
+        let table = html_carrousel_jeux(data);
         div_caroussel.appendChild(table);
     } else {
-        table = html_carrousel_jeux(["Something went wrong"])
+        let table = html_carrousel_jeux(["Something went wrong"])
         div_caroussel.appendChild(table)
     }
 }
@@ -155,14 +155,14 @@ function add_carrousel_jeux(data) {
  */
 
 function html_carrousel_jeux(data) {
-    table = document.createElement("table");
-    tr = document.createElement("tr");
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
 
     for (let i = 0; i < data.length; i++) {
-        td = document.createElement("td");   
+        let td = document.createElement("td");   
 
-        style_td = get_style_td();
-        contenu_td = td_carrousel_jeux(data[i]);
+        let style_td = get_style_td();
+        let contenu_td = td_carrousel_jeux(data[i]);
 
         td.appendChild(style_td);
         td.appendChild(contenu_td);
@@ -173,18 +173,18 @@ function html_carrousel_jeux(data) {
 }
 
 function td_carrousel_jeux(content) {
-    p = document.createElement("p");
+    let p = document.createElement("p");
 
-    contenu = document.createTextNode(content);
+    let contenu = document.createTextNode(content);
     p.appendChild(contenu);
 
     return p
 }
 
 function get_style_td() {
-    div = document.createElement("div") ;
+    let div = document.createElement("div") ;
 
-    rand = Math.random(); // 0 ou 1
+    let rand = Math.random(); // 0 ou 1
 
     if (rand > 0.5) {
         div.className = "backBlur bleu"
@@ -203,23 +203,23 @@ function get_style_td() {
  * @param {[{"pseudo" : string,"points" : int,"id" : int,"lien_pp": string}]} classement 
  */
 function add_classement(classement) {
-    table = document.createElement("table");
+    let table = document.createElement("table");
 
     for (let i = 0; i < classement.length; i++) { // parcourt de la liste ligne par ligne
-        utilisateur = classement[i];
+        let utilisateur = classement[i];
         
         // on récupère la data
-        id = utilisateur.id;
-        pfp = utilisateur.lien_pp;
-        pseudo = utilisateur.pseudo;
-        pts = utilisateur.points;
+        let id = utilisateur.id;
+        let pfp = utilisateur.lien_pp;
+        let pseudo = utilisateur.pseudo;
+        let pts = utilisateur.points;
 
         // creation tr
-        tr = create_tr_user(id, pfp, pseudo, pts);
+        let tr = create_tr_user(id, pfp, pseudo, pts);
         table.appendChild(tr);
     }
     
-    div_classement = document.querySelector(".classement");
+    let div_classement = document.querySelector(".classement");
     div_classement.appendChild(table);
 }
 
@@ -230,15 +230,15 @@ function add_classement(classement) {
  */
 
 function create_tr_user(id, pfp, pseudo, pts) {
-    tr = document.createElement("tr");
+    let tr = document.createElement("tr");
     
-    link = document.createElement("a");
+    let link = document.createElement("a");
     
-    span_pfp = document.createElement("span");
-    span_pseudo = document.createElement("span");
-    span_score = document.createElement("span");
+    let span_pfp = document.createElement("span");
+    let span_pseudo = document.createElement("span");
+    let span_score = document.createElement("span");
     
-    img_pfp = document.createElement("img");
+    let img_pfp = document.createElement("img");
 
     // data
     // img_pfp.src = pfp;
