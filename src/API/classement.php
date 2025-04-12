ffb4f7cee22cb8fcf203a0afef44543ac79998a6
synnfall <synnfall@gmail.com>
2025-04-12 15:53:06 +0200

    correction problème vérification si la méthode get renvoie un chiffre

--- src/API/classement.php
+++ src/API/classement.php
@@ -4,8 +4,12 @@ include_once __DIR__."/../CRUD/crud_classement.php";
 
 function get_classment_api($conn)
 {
-    if(isset($_GET["cl_nb_joueurs"]) && is_int($_GET["cl_nb_joueurs"]) && $_GET["cl_nb_joueurs"] > 0)
+    if(isset($_GET["cl_nb_joueurs"]) && ctype_digit($_GET["cl_nb_joueurs"]) && ( (int) $_GET["cl_nb_joueurs"] > 0 ))
     {
-        return get_lst_classement($conn, $_GET["cl_nb_joueurs"]);
+        return get_lst_classement($conn, (int) $_GET["cl_nb_joueurs"]);
+    }
+    else
+    {
+        return get_lst_classement($conn, 10);
     }
 }
