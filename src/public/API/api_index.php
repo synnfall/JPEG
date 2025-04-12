2bce9c9ac4d94c03f19f516071fa35d88fdee613
synnfall <synnfall@gmail.com>
2025-04-12 15:37:50 +0200

    corection erreur dans un chemin

--- src/public/API/api_index.php
+++ src/public/API/api_index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__."/../db/db_connect.php";
+include_once __DIR__."/../../db/db_connect.php";
 include __DIR__."/../../API/classement.php";
 include __DIR__."/../../API/games.php";
 
