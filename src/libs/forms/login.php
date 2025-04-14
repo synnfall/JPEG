e733ec6e653adaa87f5b3e2c01dffb32d9f4087a
synnfall <synnfall@gmail.com>
2025-04-14 22:31:41 +0200

    pourquoi ???

--- src/libs/forms/login.php
+++ src/libs/forms/login.php
@@ -28,7 +28,7 @@ function register($conn)
     if(isset($_POST["register"]) && isset($_POST["mdp"]) && !isset($_POST["login"]))
     {
         $users = get_user_by_name($conn, $_POST["register"]);
-        if(! $users)
+        if($users == false)
         {
             echo "test1";
             return false;
