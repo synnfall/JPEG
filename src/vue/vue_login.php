03e28f523bfae410faf2d7aefe75011df2b76ed7
synnfall <synnfall@gmail.com>
2025-04-14 18:00:55 +0200

    test

--- src/vue/vue_login.php
+++ src/vue/vue_login.php
@@ -10,7 +10,7 @@ function error_login($check_register)
     {
         $html .= "Vous ne pouvez pas vous connecter et vous enregistrer en même temps !";
     }
-    elseif($check_register==NULL)
+    elseif(isset($_POST["register"]) && $check_register==NULL)
     {
         $html .= "Votre création a réussi mais la connexion a votre compte a échoué !";
     }
