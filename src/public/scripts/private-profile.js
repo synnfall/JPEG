b474a9f4f3c27158a017fd21ae84adb566fe6280
Bouncii <lulubouncer@gmail.com>
2025-04-14 18:27:26 +0200

    boutons menu fonctionnels

--- src/public/scripts/private-profile.js
+++ src/public/scripts/private-profile.js
@@ -26,10 +26,25 @@ function button_settings_event(){
     let id_bouton = bouton.getAttribute('id');
     let coord = bouton.getBoundingClientRect();
     coord_triangle=(coord.top-445) + (coord.height/2);
+    
 
     button_settings.style.backgroundColor = "";
     button_friends.style.backgroundColor = "";
     button_stats.style.backgroundColor = "";
+
+    pages=document.querySelectorAll(".profile-page");
+
+    for (let i=0;i<pages.length;i++){
+        pages[i].classList.add("hidden");
+    }
+
+    if (id_bouton === "settings_button"){
+        pages[0].classList.remove("hidden");
+    }else if (id_bouton === "friends_button"){
+        pages[1].classList.remove("hidden");
+    }else{
+        pages[2].classList.remove("hidden");
+    }
 
     bouton.style.backgroundColor = "#FFB433";
 }
