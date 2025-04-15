118c7763d0f64a7f80506adf78d6edd37ebd01dc
Bouncii <lulubouncer@gmail.com>
2025-04-15 19:44:29 +0200

    remove triangle + implementation gear

--- src/public/scripts/private-profile.js
+++ src/public/scripts/private-profile.js
@@ -1,17 +1,6 @@
 
-const triangle = document.getElementById("cursor_triangle");
 const container = document.getElementById("container_pages_profile_buttons");
 
-var coord_triangle = -225;
-
-container.addEventListener("mousemove", (e) => {
-    const y = e.clientY - (445);
-    triangle.style.transform = `translateY(${y}px)`;
-});
-
-container.addEventListener("mouseleave", () => {
-    triangle.style.transform = `translateY(${coord_triangle}px)`;
-});
 
 const button_settings = document.querySelector("#settings_button");
 const button_friends = document.querySelector("#friends_button");
@@ -24,8 +13,6 @@ button_stats.addEventListener("click",button_settings_event);
 function button_settings_event(){
     let bouton = this;
     let id_bouton = bouton.getAttribute('id');
-    let coord = bouton.getBoundingClientRect();
-    coord_triangle=(coord.top-445) + (coord.height/2);
     
 
     button_settings.style.backgroundColor = "";
