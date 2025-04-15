46c5cc71cf5979aef932fa734e0a5794eeca6d9b
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2025-04-15 16:24:36 +0200

    fix 404

--- src/public/404.php
+++ src/public/404.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>JPEG - 404</title>
-  <link rel="stylesheet" href="./style/global.css">
   <style>
     :root {
     --primary : #151922 ;
