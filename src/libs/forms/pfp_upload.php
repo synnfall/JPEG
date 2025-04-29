e36635683453b9b397717a505eec7164fb7c68e8
synnfall <synnfall@gmail.com>
2025-04-29 16:19:49 +0200

    jsp

--- src/libs/forms/pfp_upload.php
+++ src/libs/forms/pfp_upload.php
@@ -13,12 +13,10 @@ function upload_pfp($conn){
     ];
 
     if (!isset($_FILES['pfp']) || $_FILES['pfp']['error'] !== UPLOAD_ERR_OK) {
-        echo "1";
         return false;
     }
 
     if ($_FILES['pfp']['size'] > $maxFileSize) {
-        echo "2";
         return false;
     }
 
@@ -26,13 +24,11 @@ function upload_pfp($conn){
     $mime = $finfo->file($_FILES['pfp']['tmp_name']);
 
     if (!in_array($mime, $allowedMimeTypes)) {
-        echo "3";
         return false;
     }
 
     $dimensions = getimagesize($_FILES['pfp']['tmp_name']);
     if ($dimensions === false) {
-        echo "4";
         return false;
     }
 
@@ -40,10 +36,8 @@ function upload_pfp($conn){
     $destination = __DIR__. "/../../public/img/pfp" . $newFilename;
 
     if (!move_uploaded_file($_FILES['pfp']['tmp_name'], $destination)) {
-        echo "5";
         return false;
     }
-    echo "6";
 
     return update_pfp($conn, $_SESSION["UserID"], "./img/pfp/".$newFilename);
 }
