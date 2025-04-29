2e07f5a259851b9468a7b86c56f8a1f2438a7dcf
ItsMe-Truncation <fraudulax407@gmail.com>
2025-04-29 10:23:59 +0200

    test apres debuuf 50000

--- src/API/profil/get.php
+++ src/API/profil/get.php
@@ -1,11 +1,15 @@
 <?php
-include_once __DIR__ . '/../../CRUD/crud_utilisateurs.php'; 
+ include_once __DIR__ . '/../../CRUD/crud_utilisateurs.php'; 
+ 
+ function get_utilisateur($conn, $userId) {
+ 
+ 
+ 
+     
+     $user = get_user($conn, $userId)[0];
+     
+     $date = date("d/m/Y", strtotime($user["dateCreation"] ?? "now"));
+ 
+     return json_encode(["identifiant" => $user["identifiant"], "chemin_pfp" => $user["lienPdp"], "date_join" => $date, "parties_w" => 40, "parties_l" => 21 ]);
 
-function get_utilisateur($identifant,) {
-    
-    $date = date("d/m/Y", strtotime($$_SESSION['dateCreation'] ?? "now"));
-
-    return json_encode(["identifiant" => $identifant, "chemin_pfp" => $_SESSION['lienPdp'], "date_join" => $date, "parties_w" => 40, "parties_l" => 21 ]);
-
-
-}
+ }
