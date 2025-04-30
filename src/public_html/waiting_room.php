1a44a9e9afbcac9d52e67cab32eebf9a64108b87
synnfall <synnfall@gmail.com>
2025-04-30 15:08:50 +0200

    debug

--- src/public_html/waiting_room.php
+++ src/public_html/waiting_room.php
@@ -1,5 +1,8 @@
 <?php
+error_reporting(E_ALL) ;
+ini_set( 'display_errors' , '1' ) ;
 include_once __DIR__."/../libs/session.php";
+include_once __DIR__."/../libs/lib_queue.php";
 if(! $connected)
 {
   header("Location: login.php");
