0cab688bc3f259d7c54c5de5e91befb90bc4cb33
Bouncii <lulubouncer@gmail.com>
2025-04-30 15:47:26 +0200

    fix link

--- src/public_html/games/pfc.php
+++ src/public_html/games/pfc.php
@@ -18,8 +18,8 @@ include_once __DIR__."/../libs/session.php";
   <nav>
     <ul id="navbar">
       <li class="active"><a href="../">JPEG</a></li>
-      <li><a href="../jeux">Jeux</a></li>
-      <li><a href="../classements">Classement</a></li>
+      <li><a href="../jeux.php">Jeux</a></li>
+      <li><a href="../classements.php">Classement</a></li>
     </ul>
     <ul id="userbar">
       <?php if($connected && $_SESSION["admin"]) echo '<li class="admin"><a href="../admin/admin_utilisateur.php">Admin</a></li>'; ?> <!-- a faire apparaitre si admin -->
