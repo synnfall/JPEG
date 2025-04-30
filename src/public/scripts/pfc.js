a740f3c791bfd62b58d24d3e566c8289f674a0c4
Bouncii <lulubouncer@gmail.com>
2025-04-30 14:15:07 +0200

    js choix

---
+++ src/public/scripts/pfc.js
@@ -0,0 +1,23 @@
+var liste_choix_pfc = document.querySelectorAll(".bouton_pfc")
+  
+liste_choix_pfc.forEach(button => {
+    button.addEventListener('click', choix_pfc);
+});
+
+function choix_pfc(){
+    choix = this;
+    preview = document.getElementById("choix_player1_preview");
+    console.log(preview);
+    if (choix.id === "rock_choice"){
+        preview.src="./img/icons/rock.png"
+    }
+    else if (choix.id === "paper_choice"){
+        preview.src="./img/icons/paper.png"
+    }
+    else if(choix.id === "scissors_choice"){
+        preview.src="./img/icons/scissors.png"
+    }
+    else{
+        preview.src="./img/icons/interrogation.png"
+    }
+}
