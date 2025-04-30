7ad89065e6665cbc402c2d0b46623641c8c5bafd
ItsMe-Truncation <fraudulax407@gmail.com>
2025-04-30 19:15:45 +0200

    secu login fixation : id et xss et brut force

--- src/libs/forms/login.php
+++ src/libs/forms/login.php
@@ -1,28 +1,47 @@
 <?php
 include_once __DIR__."/../../CRUD/crud_utilisateurs.php";
 
-function login($conn){
-    if(isset($_POST["login"]) && isset($_POST["mdp"]) && !isset($_POST["register"]))
-    {
-        $users = get_user_by_name($conn, htmlspecialchars($_POST["login"], ENT_QUOTES, 'UTF-8'));
-        if($users!=false && $users!=null )
-        {
-            foreach ($users as $value)
-            {
-                if(password_verify($_POST["mdp"], $value["mdp"]))
-                {
+function login($conn) {
+    if (!isset($_SESSION['login_essai'])) {
+        $_SESSION['login_essai'] = 0;
+        $_SESSION['dernier_login_essai'] = time();
+    }
+
+    
+    if ($_SESSION['login_attempts'] >= 10 && time() - $_SESSION['dernier_login_essai'] < 300) {
+        die("Trop de tentatives. Veuillez réessayer plus tard.");
+    }
+
+    if (isset($_POST["login"], $_POST["mdp"]) && !isset($_POST["register"])) {
+        $user_input = htmlspecialchars($_POST["login"], ENT_QUOTES, 'UTF-8');
+        $users = get_user_by_name($conn, $user_input);
+
+        if ($users != false && $users != null) {
+            foreach ($users as $value) {
+                if (password_verify($_POST["mdp"], $value["mdp"])) {
+                    
+                    $_SESSION['login_attempts'] = 0;
                     $_SESSION['UserID'] = $value['UserID'];
                     $_SESSION['user'] = $value['identifiant'];
                     $_SESSION['lienPdp'] = $value['lienPdp'];
                     $_SESSION['admin'] = ($value["isAdmin"] == 1);
                     $_SESSION['dateCreation'] = $value["dateCreation"];
+                    session_regenerate_id(true); 
                     return true;
                 }
             }
         }
+
+        
+        $_SESSION['login_essai'] += 1;
+        $_SESSION['dernier_login_essai'] = time();
     }
+
     return false;
 }
+
+
+
 
 function register($conn)
 {
--- src/public_html/admin/admin_utilisateur.php
+++ src/public_html/admin/admin_utilisateur.php
@@ -64,7 +64,7 @@ if (!empty($search)) {
 </head>
 <body>
     <!-- NAVBAR -->
-     <!-- marche sur le serv hein 3 -->
+     <!-- marche sur le serv hein 444 -->
     <nav>
         <ul id="navbar">
             <li><a href="../">JPEG</a></li>
