e3a66281f0d1fde37583b84b830e8d8a41ed6563
bounci <lulubouncer@gmail.com>
2025-05-01 00:13:42 +0200

    creation win-loose + fix header waiting room

--- src/public_html/win-loose.php
+++ src/public_html/win-loose.php
@@ -20,7 +20,7 @@ $token = handle_queue($conn);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>waiting room</title>
+  <title>Resultat</title>
 
   <link rel="stylesheet" href="./style/global.css">
   <link rel="stylesheet" href="./style/waiting_room.css">
