e0001a936f033b020bc312f35709befe0516f441
ItsMe-Truncation <fraudulax407@gmail.com>
2025-04-30 09:42:58 +0200

    ptit chaangement

--- src/vue/vue_admin_jeux.php
+++ src/vue/vue_admin_jeux.php
@@ -4,10 +4,10 @@ function html_table_jeux($jeux) {
     $html = "<table border='1' cellpadding='10'>";
     $html .= "<tr><th>Nom</th><th>Likes</th><th>Description</th><th>Modifier</th><th>Supprimer</th></tr>";
     foreach ($jeux as $jeu) {
-        $id = $jeu["JeuID"];
+        $id = $jeu["ID"];
         $html .= "<tr>
-            <td>{$jeu['nom']}</td>
-            <td>{$jeu['likes']}</td>
+            <td>{$jeu['nomJeux']}</td>
+            <td>{$jeu['nbLikes']}</td>
             <td>{$jeu['description']}</td>
             <td><a href='admin_jeux.php?action=update&JeuID=$id'><img src='../img/icons/edit.svg' width='30px'></a></td>
             <td><a href='admin_jeux.php?action=delete&JeuID=$id'><img src='../img/icons/delete.svg' width='30px'></a></td>
@@ -29,10 +29,10 @@ function html_form_create_jeu() {
 
 function html_form_update_jeu($jeu) {
     return "<form method='POST' action='admin_jeux.php'>
-        <label>Nom</label><input type='text' name='nom' value='{$jeu['nom']}'><br>
-        <label>Likes</label><input type='number' name='likes' value='{$jeu['likes']}'><br>
+        <label>Nom</label><input type='text' name='nom' value='{$jeu['nomJeux']}'><br>
+        <label>Likes</label><input type='number' name='likes' value='{$jeu['nbLikes']}'><br>
         <label>Description</label><textarea name='description'>{$jeu['description']}</textarea><br>
-        <input type='hidden' name='JeuID' value='{$jeu['JeuID']}'>
+        <input type='hidden' name='JeuID' value='{$jeu['ID']}'>
         <input type='hidden' name='action' value='update'>
         <input type='submit' value='Mettre à jour'>
     </form>";
