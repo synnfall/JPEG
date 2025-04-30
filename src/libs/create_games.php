947d3e3745a39eb9a5c08ec74c0e89ff863ea2fc
synnfall <synnfall@gmail.com>
2025-04-30 23:32:10 +0200

    petite coquille

--- src/libs/create_games.php
+++ src/libs/create_games.php
@@ -7,7 +7,7 @@ function create_games($conn, $userID1, $userID2, $gameID)
     $p_id = select_partie_by_name($conn, $userID1)["idPartie"];
     switch ($gameID) {
         case 3:
-            create_pfc_games($conn, $userID1, $userID2,$p_id);
+            create_pfc_games($conn, $p_id);
             break;
     }
 }
