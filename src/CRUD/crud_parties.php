866b9ff950bd50817fd53933e6eae6b0b42bd123
synnfall <synnfall@gmail.com>
2025-04-30 10:41:21 +0200

    fin crud partie

--- src/CRUD/crud_parties.php
+++ src/CRUD/crud_parties.php
@@ -7,3 +7,70 @@ function getResultList($result) {
     }
     return $list;
 }
+
+function generateToken() {
+    return bin2hex(random_bytes(32));
+}
+
+function create_partie($conn, $gameID, $userID1, $userID2) {
+
+    $token1 = generateToken();
+    $token2 = generateToken();
+
+    $sql = "INSERT INTO `Parties`(`idPartie`, `gameID`, `IDUser1`, `IDUser2`, `token1`, `token2`, `date`) VALUES (null,?,?,?,?,?,NOW())";
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if (!$stmt) {
+        echo "Erreur de préparation : " . mysqli_error($conn);
+        return false;
+    }
+
+    mysqli_stmt_bind_param($stmt, "iiiss", $gameID, $userID1, $userID2, $token1, $token2);
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return $result;
+}
+
+function clean_partie($conn) {
+    $sql = "DELETE FROM `Parties` WHERE `date` < (NOW() - INTERVAL 6 HOUR)";
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if (!$stmt) {
+        echo "Erreur de préparation : " . mysqli_error($conn);
+        return false;
+    }
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return $result;
+}
+
+function update_partie($conn, $idPartie) {
+    $sql = "UPDATE `Parties` SET `date` = NOW() WHERE idPartie = ?";
+    
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if (!$stmt) {
+        echo "Erreur de préparation : " . mysqli_error($conn);
+        return false;
+    }
+
+    mysqli_stmt_bind_param($stmt, "i", $idPartie);
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return $result;
+}
+
+function delete_partie($conn, $idPartie)
+{
+    $sql = "DELETE FROM `Parties` WHERE `idPartie` = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if (!$stmt) {
+        echo "Erreur de préparation : " . mysqli_error($conn);
+        return false;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $idPartie);
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return $result;
+}
