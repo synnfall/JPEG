c606c2a430de742d30953dffad1b57214775f8f0
synnfall <synnfall@gmail.com>
2025-04-30 22:21:27 +0200

    retourner a la partie si elle n'est pas fini

--- src/libs/lib_queue.php
+++ src/libs/lib_queue.php
@@ -6,7 +6,7 @@ function handle_queue($conn){
     $partie = est_en_partie($conn, $_SESSION["UserID"]);
     if($partie)
     {
-        header("Location: games/".name_games($conn, $partie));
+        header("Location: games/".name_games($conn, $partie).".php"."?idPartie=".select_partie_by_name($conn, $_SESSION["UserID"])["idPartie"]);
         exit;
     }
     delete_queue($conn, $_SESSION["UserID"]);
