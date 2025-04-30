edb1a485eb267fe89ccad0a07e64b7d0b793c717
ItsMe-Truncation <fraudulax407@gmail.com>
2025-04-30 11:22:19 +0200

    modif crud

--- src/CRUD/crud_utilisateurs.php
+++ src/CRUD/crud_utilisateurs.php
@@ -10,7 +10,7 @@ function getResultList($result) {
 
 function create_user($conn, $identifiant, $password, $lien_PDP = "./img/pfp/default_pfp.jpg") {
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $sql = "INSERT INTO Utilisateurs (identifiant, mdp, lienPdp, isAdmin) VALUES (?, ?, ?, 0)";
+    $sql = "INSERT INTO Utilisateurs (identifiant, mdp, lienPdp, isAdmin, parties_w, partie_l) VALUES (?, ?, ?, 0, 0, 0)";
     $stmt = mysqli_prepare($conn, $sql);
 
     if (!$stmt) {
@@ -39,9 +39,9 @@ function update_pfp($conn, $user_id, $lien_PDP) {
     return $result;
 }
 
-function update_user($conn, $user_id, $identifiant, $password, $lien_PDP) {
+function update_user($conn, $user_id, $identifiant, $password, $lien_PDP, $parties_w = 0, $parties_l = 0) {
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $sql = "UPDATE Utilisateurs SET identifiant = ?, mdp = ?, lienPdp = ? WHERE UserID = ?";
+    $sql = "UPDATE Utilisateurs SET identifiant = ?, mdp = ?, lienPdp = ?, parties_w = ?, parties_l = ? WHERE UserID = ?";
     $stmt = mysqli_prepare($conn, $sql);
 
     if (!$stmt) {
@@ -49,7 +49,7 @@ function update_user($conn, $user_id, $identifiant, $password, $lien_PDP) {
         return false;
     }
 
-    mysqli_stmt_bind_param($stmt, "sssi", $identifiant, $hashed_password, $lien_PDP, $user_id);
+    mysqli_stmt_bind_param($stmt, "sssiii", $identifiant, $hashed_password, $lien_PDP, $parties_w, $parties_l, $user_id);
     $result = mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     return $result;
