2d52bdc5302e4b83195a735ebb7b740b4afb2cb5
ItsMe-Truncation <fraudulax407@gmail.com>
2025-05-01 15:13:07 +0200

    je sais pas ce que c'est un cheminn rellatif apparament

--- src/API/jeuxLike.php
+++ src/API/jeuxLike.php
@@ -1,6 +1,6 @@
 <?php
- include_once __DIR__ . '/../../CRUD/crud_jeux.php'; 
- include_once __DIR__ . '/../../CRUD/crud_jeuxlike.php'; 
+ include_once __DIR__ . '/../CRUD/crud_jeux.php'; 
+ include_once __DIR__ . '/../CRUD/crud_jeuxlike.php'; 
 
 
 
