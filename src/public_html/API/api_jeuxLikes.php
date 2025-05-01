43bf5217ffabb887c0ab8a0b9ac3219266350c12
ItsMe-Truncation <fraudulax407@gmail.com>
2025-05-01 15:10:35 +0200

    j'ai remonte trop loin ...

--- src/public_html/API/api_jeuxLikes.php
+++ src/public_html/API/api_jeuxLikes.php
@@ -1,13 +1,13 @@
 <?php
 
-include_once __DIR__ . '/../../../API/jeuxLike.php';
-include_once __DIR__ . '/../../../db/db_connect.php';
-include_once __DIR__ . '/../../../libs/session.php';
+include_once __DIR__ . '/../../API/jeuxLike.php';
+include_once __DIR__ . '/../../db/db_connect.php';
+include_once __DIR__ . '/../../libs/session.php';
 
-header('Content-Type: application/json');
+
 $JeuxID = intval($_POST['id_jeux']);
 $UserID = $_SESSION["UserID"];
 
 $result = add_likes($conn, $UserID, $JeuxID);
-
+header('Content-Type: application/json');
 echo $result; 
