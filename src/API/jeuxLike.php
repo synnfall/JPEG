5baf82b58fa39b12a221122ba1ce52ec6d382779
ItsMe-Truncation <fraudulax407@gmail.com>
2025-05-01 14:43:05 +0200

    ajout api like jeu

---
+++ src/API/jeuxLike.php
@@ -0,0 +1,34 @@
+<?php
+ include_once __DIR__ . '/../../CRUD/crud_jeux.php'; 
+ include_once __DIR__ . '/../../CRUD/crud_jeuxlike.php'; 
+
+
+
+ function verif_pas_encore_like($conn, $UserID, $JeuxID) {
+
+    $likes = get_LikeJeux_user($conn, $UserID);
+    foreach ($likes as $like) {
+        if ($like["JeuxID"] == $JeuxID) {
+            return false;
+            
+        }
+    }   
+    return true;
+}
+
+
+function add_likes($conn, $UserID, $JeuxID){
+    if (verif_pas_encore_like($conn, $UserID, $JeuxID)) {
+        $info_jeux = get_jeux($conn, $JeuxID);
+        $nb_likes = $info_jeux["nbLikes"] + 1; 
+        create_LikeJeux($conn, $JeuxID, $UserID);
+
+        update_jeux($conn, $JeuxID, $info_jeux["nomJeux"], $nb_likes );
+        return json_encode(["result" => true, "message" => "Votre like a bien été rajouté au jeu"]); 
+    }
+    else {
+
+        return json_encode(["result" => false, "message" => "Vous avez deja like ce jeu"]);
+    }
+    
+}
---
+++ src/public_html/API/api_jeuxLikes.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once __DIR__ . '/../../../API/jeuxLike.php';
+include_once __DIR__ . '/../../../db/db_connect.php';
+include_once __DIR__ . '/../../../libs/session.php';
+
+
+$JeuxID = intval($_POST['id_jeux']);
+$UserID = $_SESSION["UserID"];
+
+$result = add_likes($conn, $UserID, $JeuxID);
+
+echo $result; 
