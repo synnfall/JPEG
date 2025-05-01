5d5b51b13ca8c551a64fa7b207c7fb0e4c58aad9
synnfall <synnfall@gmail.com>
2025-05-01 19:45:38 +0200

    solution a tout nos problème

--- src/db/db_connect.php
+++ src/db/db_connect.php
@@ -2,7 +2,7 @@
 include "db_param.php";
 
 $conn = mysqli_connect($hosts, $user,$passwd, $db_name, $port);
-mysqli_query($conn, "SET time_zone = 'Europe/Paris'");
+//mysqli_query($conn, "SET time_zone = 'Europe/Paris'"); //décommentez serv profs
 if(!$conn)
 {
     echo "Même ici tu ne gagne pas ! ( La connexion à la BDD a échoué )";
