16faf94e99aa8f0b83354ed87be5939db2656b5a
synnfall <synnfall@gmail.com>
2025-05-04 16:07:25 +0200

    pour que le nom d'utilisateur soit update en direct en cas de chagement

--- src/libs/forms/update_profile.php
+++ src/libs/forms/update_profile.php
@@ -4,5 +4,9 @@ include_once __DIR__."/../../CRUD/crud_utilisateurs.php";
 
 function change_name_and_passwd($conn, $name, $passwd)
 {
-    return update_user($conn, $_SESSION["UserID"], $name, $passwd, $_SESSION["lienPdp"]);
+    if(update_user($conn, $_SESSION["UserID"], $name, $passwd, $_SESSION["lienPdp"])){
+        $_SESSION['user'] = $name;
+        return true;
+    }
+    return false;
 }
